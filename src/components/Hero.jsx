ec06c52d102265851aafb023702acf76d93c66d8
Max Yuan <max.yuan@yale.edu>
2023-10-20 09:21:49 -0400

    hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { Text } from '../ui';
+import { Icons, Row, Text } from '../ui';
 
 const Container = styled.div`
   height: 100vh;
@@ -13,10 +13,67 @@ const Container = styled.div`
   background-repeat: no-repeat;
 `;
 
+const Content = styled.div`
+  margin: auto;
+  width: 100%;
+  max-width: 1144px;
+  height: 100%;
+  padding: 50px 0;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Navbar = styled(Row)`
+  justify-content: space-between;
+`;
+
+const NavButtons = styled(Row)`
+  > :not(:last-child) {
+    margin-right: 40px;
+  }
+
+  > * {
+    cursor: pointer;
+    transition: color 0.2s ease-in-out;
+
+    &:hover {
+      color: ${({ theme }) => theme.palette.gold};
+    }
+  }
+`;
+
+const Body = styled.div`
+  margin: auto auto 15vh 0;
+
+  > :not(:first-child) {
+    margin-top: 25px;
+  }
+
+  ${Text} {
+    max-width: 840px;
+  }
+`;
+
 export const Hero = () => {
   return (
     <Container>
-      <Text type="display-h1">HERO</Text>
+      <Content>
+        <Navbar>
+          <Icons.logo width={90} height={90} />
+          <NavButtons>
+            <Text type="display-h2">About</Text>
+            <Text type="display-h2">Technology</Text>
+            <Text type="display-h2">Contact Us</Text>
+          </NavButtons>
+        </Navbar>
+        <Body>
+          <Icons.logoWords height={180} width={488} />
+          <Text type="display-h2" weight={500}>
+            Rethinking the chemical and material economy to secure our industry,
+            our country, and our planet.
+          </Text>
+        </Body>
+      </Content>
     </Container>
   );
 };
